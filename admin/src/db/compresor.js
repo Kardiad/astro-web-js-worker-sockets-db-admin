283e6807a30a6a285358b5a874aaaad79ad0fc73
Kardiad <mrakami98@gmail.com>
2025-12-09 20:00:08 +0100

    compresor

---
+++ admin/src/db/compresor.js
@@ -0,0 +1,27 @@
+export class Compresor {
+
+    async getImage(url){
+        
+    }
+
+    zipImage(blob) {
+        return new Promise((res, rej) => {
+            const canvas = document.createElement('canvas');
+            const imagen = new Image();
+            imagen.onload = () => {
+                canvas.width = imagen.width;
+                canvas.height = imagen.height;
+                canvas.getContext('2d').drawImage(imagen, 0, 0);
+                canvas.toBlob((blob) => {
+                    if (blob === null) {
+                        return rej(blob);
+                    } else {
+                        blob.name = 'a.png';
+                        res(blob);
+                    }
+                }, 'image/webp', 0.4);
+            }
+            imagen.src = URL.createObjectURL(blob);
+        })
+    }
+}
