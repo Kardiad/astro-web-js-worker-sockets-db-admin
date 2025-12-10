49d34506bf477b30aaf6c2ca174182d61bc6a34f
Kardiad <mrakami98@gmail.com>
2025-12-10 11:39:26 +0100

    implements image field

--- admin/src/db/compresor.js
+++ admin/src/db/compresor.js
@@ -1,27 +1,36 @@
 export class Compresor {
 
-    async getImage(url){
-        
+    async getImage(url) {
+        return fetch(url);
     }
 
-    zipImage(blob) {
-        return new Promise((res, rej) => {
-            const canvas = document.createElement('canvas');
-            const imagen = new Image();
-            imagen.onload = () => {
-                canvas.width = imagen.width;
-                canvas.height = imagen.height;
-                canvas.getContext('2d').drawImage(imagen, 0, 0);
-                canvas.toBlob((blob) => {
-                    if (blob === null) {
-                        return rej(blob);
-                    } else {
-                        blob.name = 'a.png';
-                        res(blob);
-                    }
-                }, 'image/webp', 0.4);
-            }
-            imagen.src = URL.createObjectURL(blob);
-        })
+    makeBase64(blob) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result); // contiene el base64
+            reader.onerror = reject;
+            reader.readAsDataURL(blob); // convierte a base64
+        });
     }
-}
+
+        zipImage(blob) {
+            return new Promise((res, rej) => {
+                const canvas = document.createElement('canvas');
+                const imagen = new Image();
+                imagen.onload = () => {
+                    canvas.width = imagen.width;
+                    canvas.height = imagen.height;
+                    canvas.getContext('2d').drawImage(imagen, 0, 0);
+                    canvas.toBlob((blob) => {
+                        if (blob === null) {
+                            return rej(blob);
+                        } else {
+                            blob.name = 'a.png';
+                            res(blob);
+                        }
+                    }, 'image/webp', 0.4);
+                }
+                imagen.src = URL.createObjectURL(blob);
+            })
+        }
+    }
